50d659b367cbff28b54af1f98c227f93734bfd6a
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-08-01 11:18:30 +0200

    Implements promise and RemoteModel methods.

--- tests.js
+++ tests.js
@@ -9,11 +9,13 @@ var host = "http://localhost:8000";
 
 // Test Daybed.getToken and hello
 function testHello(done) {
-  Daybed.getToken(host, hello);
+  Daybed.getToken(host)
+    .then(hello);
 
-  function hello(err, resp) {
+  function hello(resp) {
     var session = new Daybed.Session(host, resp.credentials);
-    session.hello(done);
+    session.hello()
+      .then(done);
   }
 }
 
@@ -21,10 +23,10 @@ function testHello(done) {
 function testModelSessionApi(done) {
   var session;
 
-  Daybed.getToken(host, defineAgeModel);
+  Daybed.getToken(host)
+    .then(defineAgeModel);
 
-  function defineAgeModel(err, resp) {
-    if (err) throw new Error(err);
+  function defineAgeModel(resp) {
     session = new Daybed.Session(host, resp.credentials);
     session.addModel("age", {
       "title": "simple",
@@ -32,86 +34,87 @@ function testModelSessionApi(done) {
       "fields": [{"name": "age",
                   "type": "int",
                   "required": false}]
-    }, [{"age": 42}], getAgeModel);
+    }, [{"age": 42}])
+      .then(getAgeModel);
   }
 
-  function getAgeModel(err, resp) {
-    if (err) throw new Error(err);
+  function getAgeModel(resp) {
     done("Add model", resp);
-    session.getModel("age", addRecord);
+    session.getModel("age")
+      .then(addRecord);
   }
 
-  function addRecord(err, resp) {
-    if (err) throw new Error(err);
+  function addRecord(resp) {
     done("Get model", resp);
-    session.addRecord("age", {"id": "123456", "age": 25}, getRecord);
+    session.addRecord("age", {"id": "123456", "age": 25})
+      .then(getRecord);
   }
 
-  function getRecord(err, resp) {
-    if (err) throw new Error(err);
+  function getRecord(resp) {
     done("Add record", resp);
-    session.getRecord("age", "123456", patchRecord);
+    session.getRecord("age", "123456")
+      .then(patchRecord);
   }
 
-  function patchRecord(err, resp) {
-    if (err) throw new Error(err);
+  function patchRecord(resp) {
     done("Get record", resp);
-    session.patchRecord("age", "123456", {"age": 41}, deleteRecord);
+    session.patchRecord("age", "123456", {"age": 41})
+      .then(deleteRecord);
   }
 
-  function deleteRecord(err, resp) {
-    if (err) throw new Error(err);
+  function deleteRecord(resp) {
     done("Patch record", resp);
-    session.deleteRecord("age", "123456", addOtherRecord);
+    session.deleteRecord("age", "123456")
+      .then(addOtherRecord);
   }
 
-  function addOtherRecord(err, resp) {
-    if (err) throw new Error(err);
+  function addOtherRecord(resp) {
     done("Delete record", resp);
-    session.addRecord("age", {"id": "123456", "age": 25}, getRecords);
+    session.addRecord("age", {"id": "123456", "age": 25})
+      .then(getRecords);
   }
 
-  function getRecords(err, resp) {
-    if (err) throw new Error(err);
+  function getRecords(resp) {
     done("Add record", resp);
-    session.getRecords("age", deleteRecords);
+    session.getRecords("age")
+      .then(deleteRecords);
   }
 
-  function deleteRecords(err, resp) {
-    if (err) throw new Error(err);
+  function deleteRecords(resp) {
     done("Get records", resp);
-    session.deleteRecords("age", getDefinition);
+    session.deleteRecords("age")
+      .then(getDefinition);
   }
 
   function getDefinition(err, resp) {
-    if (err) throw new Error(err);
     done("Delete records", resp);
-    session.getDefinition("age", getPermissions);
+    session.getDefinition("age")
+      .then(getPermissions);
+
   }
 
-  function getPermissions(err, resp) {
-    if (err) throw new Error(err);
+  function getPermissions(resp) {
     done("Model definition", resp);
-    session.getPermissions("age", deleteModel);
+    session.getPermissions("age")
+      .then(deleteModel);
   }
 
-  function deleteModel(err, resp) {
-    if (err) throw err;
+  function deleteModel(resp) {
     done("Model permissions", resp);
-    session.deleteModel("age", function(err, resp) {
-      if (err) throw err;
-      done("Delete model", resp);
-    });
+    session.deleteModel("age")
+      .then(function(resp) {
+        done("Delete model", resp);
+      });
   }
 }
 
 // Test Permissions API
 function testPermissionsAPI(done) {
   var session, session2, tokenId, tokenId2;
-  Daybed.getToken(host, addAgesModel);
+  Daybed.getToken(host)
+    .then(addAgesModel);
 
-  function addAgesModel(err, resp) {
-    if (err) throw new Error(err);
+  function addAgesModel(resp) {
     session = new Daybed.Session(host, resp.credentials);
     tokenId = resp.credentials.id;
 
@@ -121,18 +124,16 @@ function testPermissionsAPI(done) {
       "fields": [{"name": "age",
                   "type": "int",
                   "required": false}]
-    }, getOtherToken);
+    }).then(getOtherToken);
   }
 
-  function getOtherToken(err, resp) {
-    if (err) throw new Error(err);
+  function getOtherToken(resp) {
     done("Add model", resp);
-
-    Daybed.getToken(host, putPermissions);
+    Daybed.getToken(host)
+      .then(putPermissions);
   }
 
-  function putPermissions(err, resp) {
-    if (err) throw new Error(err);
+  function putPermissions(resp) {
     session2 = new Daybed.Session(host, resp.credentials);
     tokenId2 = resp.credentials.id;
 
@@ -141,47 +142,47 @@ function testPermissionsAPI(done) {
     permissions[tokenId2] = ["read_definition", "create_record", "read_own_records",
                       "update_own_records", "delete_own_records"];
 
-    session.putPermissions("ages", permissions, patchPermissions);
+    session.putPermissions("ages", permissions)
+      .then(patchPermissions);
   }
 
-  function patchPermissions(err, resp) {
-    if (err) throw new Error(err);
+  function patchPermissions(resp) {
     done("Put Permissions", resp);
-    session.patchPermissions("ages", {"Everyone": ["read_definition"]}, addRecord);
+    session.patchPermissions("ages", {"Everyone": ["read_definition"]})
+      .then(addRecord);
   }
 
-  function addRecord(err, resp) {
-    if (err) throw new Error(err);
+  function addRecord(resp) {
     done("Patch Permissions", resp);
-    session.addRecord("ages", {"age": 25}, addRecord2);
+    session.addRecord("ages", {"age": 25})
+      .then(addRecord2);
   }
 
-  function addRecord2(err, resp) {
-    if (err) throw new Error(err);
+  function addRecord2(resp) {
     done("Add tokenId record", resp);
-    session2.addRecord("ages", {"age": 42}, getRecords);
+    session2.addRecord("ages", {"age": 42})
+      .then(getRecords);
   }
 
-  function getRecords(err, resp) {
-    if (err) throw new Error(err);
+  function getRecords(resp) {
     done("Add tokenId2 record", resp);
-    session.getRecords("ages", getRecords2);
+    session.getRecords("ages")
+      .then(getRecords2);
   }
 
-  function getRecords2(err, resp) {
-    if (err) throw new Error(err);
+  function getRecords2(resp) {
     done("Get tokenId record", resp);
-    session2.getRecords("ages", deleteAgesModel);
+    session2.getRecords("ages")
+      .then(deleteAgesModel);
   }
 
-  function deleteAgesModel(err, resp) {
-    if (err) throw new Error(err);
+  function deleteAgesModel(resp) {
     done("Get tokenId2 record", resp);
 
-    session.deleteModel("ages", function(err, resp) {
-      if (err) throw new Error(err);
-      done("Delete ages", resp);
-    });
+    session.deleteModel("ages")
+      .then(function(resp) {
+        done("Delete ages", resp);
+      });
   }
 }
 
@@ -191,13 +192,13 @@ function testModelApi(done) {
   var session,
       books;
 
-  Daybed.getToken(host, defineBookModel);
+  Daybed.getToken(host)
+    .then(defineBookModel);
 
-  function defineBookModel(err, resp) {
-    if (err) throw new Error(err);
+  function defineBookModel(resp) {
     session = new Daybed.Session(host, resp.credentials);
 
-    books = new Daybed.Model("books", {
+    books = new Daybed.Model(session, "books", {
       title: 'book',
       description: "The list of books to read",
       fields: [
@@ -217,7 +218,7 @@ function testModelApi(done) {
           label: "Summary"
         }
       ]
-    }, session);
+    });
 
     books.add(
       {
@@ -227,23 +228,34 @@ function testModelApi(done) {
       }
     );
 
-    books.save(getBookModel);
+    books
+      .save()
+      .then(getBookModel);
   }
 
   function getBookModel(err, resp) {
-    session.getModel("books", deleteBookModel);
+    books = new Daybed.Model(session, "books");
+    books.load()
+      .then(function() {
+        done(books.definition());
+        done(books.records());
+        deleteBookModel();
+      }
+    );
   }
 
-  function deleteBookModel(err, resp) {
-    done(resp);
-    books.delete(done);
+  function deleteBookModel() {
+    books.delete()
+      .then(done);
   }
 }
 
 function testSpore(done) {
-  Daybed.spore(host, done);
+  Daybed.spore(host)
+    .then(done);
 }
 
 function testFields(done) {
-  Daybed.availableFields(host, done);
+  Daybed.availableFields(host)
+    .then(done);
 }
