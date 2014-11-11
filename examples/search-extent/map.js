cafdc160511783b0aebcce1d33e187812a09985f
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-11-11 18:03:42 +0100

    Formatting

--- examples/search-extent/map.js
+++ examples/search-extent/map.js
@@ -1,61 +1,10 @@
 (function () {
 
-  var server = 'http://localhost:8000';
+  var server = 'https://daybed.io';
   var model = 'daybed:examples:search:extent';
 
-
-  var _session;
-  function getSession() {
-    _session = _session || Daybed.startSession(server, {anonymous: true});
-    return _session;
-  }
-
-
-  function loadGeoJson() {
-    return getSession()
-      .then(function (session) {
-        return session.getRecords(model, {
-          format: 'application/vnd.geo+json',
-        });
-      });
-  }
-
-
-  function searchExtent(bbox) {
-    var query = {
-      size: 500,
-      query: {
-
-          filtered: {
-          query: {
-            match_all: {},
-          },
-          filter: {
-            geo_bounding_box : {
-              location: {
-                top: bbox.getNorthWest().lat,
-                left: bbox.getNorthWest().lng,
-                bottom: bbox.getSouthEast().lat,
-                right: bbox.getSouthEast().lng
-              }
-            }
-          }
-        }
-      }
-    };
-
-    return getSession()
-      .then(function (session) {
-        return session.searchRecords(model, query);
-      })
-      .then(function (response) {
-        return response.hits.hits.map(function (r) {
-          return r._source;
-        });
-      });
-  }
-
-
+  // Minimalistic React component
+  // showing records within map extent
   var ResultsList = React.createClass({
     getInitialState: function() {
       return {results: []};
@@ -79,7 +28,7 @@
   });
 
 
-
+  // Basic map initialization
   var map = L.map('map')
     .setView([41.8, 12.5], 9);
 
@@ -91,20 +40,75 @@
    .addTo(map)
    .show();
 
-   React.render(
+  // Render results, with reference on map
+  React.render(
     React.createElement(ResultsList, {map: map}),
     document.getElementById('results'));
 
-
+  // Show all records on map
   loadGeoJson()
     .then(function (geojson) {
       L.geoJson(geojson, {
         pointToLayer: function(featureData, latlng) {
-          return L.circle(latlng, 30, {color: 'purple', fillOpacity: 1.0});
+          return L.circle(latlng, 30, {
+            color: 'purple',
+            fillOpacity: 1.0
+          });
         }
       }).addTo(map);
     });
 
 
+  // Re-usable session Promise
+  var _session;
+  function getSession() {
+    _session = _session || Daybed.startSession(server, {anonymous: true});
+    return _session;
+  }
+
+  // Helper to fetch records as GeoJSON
+  function loadGeoJson() {
+    return getSession()
+      .then(function (session) {
+        return session.getRecords(model, {
+          format: 'application/vnd.geo+json',
+        });
+      });
+  }
+
+  // Helper to run ElasticSearch query on Leaflet LatLngBounds object
+  function searchExtent(bbox) {
+    var query = {
+      size: 500,
+      query: {
+        filtered: {
+          query: {
+            match_all: {},
+          },
+          filter: {
+            geo_bounding_box : {
+              location: {
+                top: bbox.getNorthWest().lat,
+                left: bbox.getNorthWest().lng,
+                bottom: bbox.getSouthEast().lat,
+                right: bbox.getSouthEast().lng
+              }
+            }
+          }
+        }
+      }
+    };
+
+    return getSession()
+      .then(function (session) {
+        return session.searchRecords(model, query);
+      })
+      .then(function (response) {
+        // Flatten records from query results
+        return response.hits.hits.map(function (r) {
+          return r._source;
+        });
+      });
+  }
 
 })();
